f324c1fb0626e3cc2014c4d9806985c596c8902e
arjun-krishna <arjunkrishna1306@gmail.com>
2020-12-02 23:53:38 -0500

    Add navbar to frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div class="navbar">
+          zmaps | World Anti-Zombie Federation
+          <br/>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
